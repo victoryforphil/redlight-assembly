065a655aec40251db71af4170d1ae8cc67f926b7
Alex Carter <VictoryForPhil@gmail.com>
2021-12-16 00:40:37 +0000

    working print message

--- pi/redlight.s
+++ pi/redlight.s
@@ -1,6 +1,9 @@
+@----------------------------------
 
 .data
 
+@ See /usr/include/arm-linux-gnueabihf/asm/unistd.h
+@ See /usr/include/arm-linux-gnueabihf/bits/fcntl-linux.h
     .equ open,       5
          .equ Rd,   00
     .equ read,       3
@@ -9,21 +12,17 @@
 
 @----------------------------------
 
+welcome_mesage:
+    .asciz "Welcome to Redlight in ARM ASM!\nTest\n"
+len = . - welcome_mesage
 .balign 4
 dir_file:
     .asciz "/dev/urandom"
 
 .balign 4
-Open:
-    .word dir_file, Rd, open
-
-.balign 4
 Buf:
     .word 0 
 
-.balign 4
-Read:
-    .word Buf, 1, read
 
 .balign 4
 format:
@@ -33,25 +32,26 @@ format:
 
 .text
 
-.global main, printf, sleep
+.global main, printf
 
 main:
 
     push   {r4, r5, r7, lr}     @ folowing AAPCS
-    #mov r0, #5
-   # bl      sleep
-    bal    _genradom
 
-    mov r0, r1
-    bal _delay
-                   @ C() print byte
-    mov    r0, #0               @ 0 = success
+    bl      _print_welcome
+    mov     r0, #0               @ 0 = success
+    mov     r7, #1
+    svc     #0
 
-/* Delays Seconds in R0 */
-_delay:
-    bl sleep
 
-/* Generates a random 1 byte number and stores the result  in R1 */
+_print_welcome:
+    push {lr}
+    ldr      r0, =welcome_mesage
+    bl      printf
+    ldr r2, =len
+    pop {pc}
+
+/* Generates a random 1 byte number and stores the result in the address in R1 */
 _genradom:
     ldr     r0, =dir_file           @ Save the pointer to the path (/dev/urandom) into R0
     mov     r7, #5                  @ Save the sycall 5 (open) to R7 for SVC Call
@@ -70,8 +70,9 @@ _genradom:
 
     ldr    r0, =format          @ adress of format
     ldr    r1, =Buf             @ addr of byte red
-    ldr    r1, [r1]             @ load byte
-    
+    ldr    r1, [r1]             @ load byte 
+    #mov    r5, lr
+    bx     lr
 exit:
 
      pop    {r4, r5, r7, lr}     @ folowing AAPCS
